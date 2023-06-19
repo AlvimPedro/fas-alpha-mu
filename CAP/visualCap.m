945c5ed08843f1f6ca206cfdc9b3bea9d9989826
AlvimPedro <pedroalvim2000@hotmail.com>
2023-06-19 09:50:21 -0300

    Capacidade

--- CAP/visualCap.m
+++ CAP/visualCap.m
@@ -5,16 +5,16 @@ clc;
 set(0,'defaulttextinterpreter','latex');
 set(groot,'defaultAxesTickLabelInterpreter','latex');
 
-c1 = load('CAP\data10dB\CAP05.mat');
+c1 = load('CAP\dataJakes10dB\CAP05.mat');
 semilogx(c1.N_ports, c1.C(:));
 hold on;
-c2 = load('CAP\data10dB\CAP2.mat');
+c2 = load('CAP\dataJakes10dB\CAP2.mat');
 semilogx(c2.N_ports, c2.C(:));
 hold on;
-c3 = load('CAP\data10dB\CAP5.mat');
+c3 = load('CAP\dataJakes10dB\CAP5.mat');
 semilogx(c3.N_ports, c3.C(:));
 hold on;
-c4 = load('CAP\data10dB\CAP10.mat');
+c4 = load('CAP\dataJakes10dB\CAP10.mat');
 semilogx(c4.N_ports, c4.C(:));
 hold on;
 
@@ -45,5 +45,5 @@ legend('$\alpha$ = 0.5',...
        '$\alpha$ = 10',...
        'FontSize', 12, 'location','southeast')
 
-savefig('figs\CapFig2.fig')
+savefig('figs\CapJakesFig2.fig')
 hold off
